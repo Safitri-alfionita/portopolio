3de36a090805ae88a95c792a95b45bc04c0a9cb2
Safitri-alfionita <safitrialft@gmail.com>
2025-05-19 06:15:26 +0700

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Projects from './Components/Projects';
+import Skill from './Components/Skill';
+import Education from './Components/Educations';
+import Contacts from './Components/Contacts';
+import Footers from './Components/Footers';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Skill />
+      <Education />
+      <Projects />
+      <Contacts />
+      <Footers />
+    </>
+  );
+}
+
+export default App;
